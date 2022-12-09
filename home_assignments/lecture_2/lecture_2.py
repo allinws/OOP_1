ef66616948c9b0c0a7ed28daa373cca4bfa23e08
Alexander Lindgren <alexander.lindgren@willandskill.se>
2022-12-09 13:07:53 +0100

    updated lecture_2 with a list of tuples

--- home_assignments/lecture_2/lecture_2.py
+++ home_assignments/lecture_2/lecture_2.py
@@ -1,7 +1,7 @@
 car_models = [
     {'model': 'Model X', 'weight': 1100}, 
     {'model': 'Model S', 'weight': 1300}, 
-    {'asasdas': 'Model Y', 'weight': 1700}, 
+    {'model': 'Model Y', 'weight': 1700}, 
     {'model': 'Model 3', 'weight': 1900},
 ]
 
@@ -21,17 +21,19 @@ class CarPrinterClass(CarPriceClass):
 
     def __init__(self, car_prices_list, car_models):
         CarPriceClass.__init__(self, car_prices_list)
-        # super().__init__(car_prices_list)
+        # super().__init__(car_prices_list) # Går lika bra
         self.car_models = car_models
     
     def print_car_prices(self):
+        car_model_tuples = []
         dicts_missing_key = []
         for model in self.car_models:
             model_name = model.get('model')
             dicts_missing_key.append(model)
             price = self.return_car_price(model_name)
             car_tuple = (model_name, price)
-            print(car_tuple)
+            car_model_tuples.append(car_tuple)
+        print(car_model_tuples)
 
 
 car_instance = CarPrinterClass(car_models=car_models, car_prices_list=car_prices_list)
